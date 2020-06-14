305e7f17e464df03dc73d1ee18167ad72a652ce1
Francisco Pariona <fcoparionavega@gmail.com>
2020-06-14 16:13:20 -0500

    Initial commit

---
+++ Assets/Scripts/DirectionalLightController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLightController : MonoBehaviour
+{
+    [SerializeField] private float _rotationStep;
+
+    private Vector3 rotationVec;
+    
+    void Start()
+    {
+        rotationVec = new Vector3(0, _rotationStep, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateLight();
+    }
+
+    void RotateLight()
+    {
+        transform.Rotate(rotationVec);
+    }
+}
---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    [SerializeField] private float _stepLength;
+
+    private Vector3 _translationVec;
+
+    private void Start()
+    {
+        _translationVec = new Vector3(_stepLength,0,0);
+    }
+
+    void Update()
+    {
+        AutomaticMovement();
+    }
+
+    void AutomaticMovement()
+    {
+        transform.Translate(_translationVec);
+    }
+}
